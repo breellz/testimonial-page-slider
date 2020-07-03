bb88b864c52d5cef0ce4f214147f63518c444a7d
Bassit <barseetbrown@gmail.com>
2020-07-03 09:52:53 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+const showSlides = (n) => {
+  let i;
+  const slides = document.getElementsByClassName("slides");
+  const dots = document.getElementsByClassName("arrow-control");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+const plusSlides = (n) => {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+const currentSlide = (n) => {
+  showSlides(slideIndex = n);
+}
